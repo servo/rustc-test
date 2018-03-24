ceacc32b75fad29456545904138b9e7d632a0b86
messense <messense@icloud.com>
2018-03-24 16:32:52 +0800

    Remove panic_unwind feature

--- src/lib.rs
+++ src/lib.rs
@@ -30,12 +30,10 @@
 #![cfg_attr(feature = "asm_black_box", feature(asm))]
 #![feature(fnbox)]
 #![feature(set_stdio)]
-#![feature(panic_unwind)]
 
 extern crate getopts;
 extern crate term;
 extern crate libc;
-extern crate panic_unwind;
 
 pub use self::TestFn::*;
 pub use self::ColorConfig::*;
